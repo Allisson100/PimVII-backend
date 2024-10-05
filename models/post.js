0322163cbc3b69efa8bca31859ca1d97541e9d17
Allisson100 <allisson.boiane@outlook.com>
2024-10-05 11:36:38 -0300

    fix: db/addPostUuid

--- models/post.js
+++ models/post.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
 const postSchema = new mongoose.Schema(
   {
     id: { type: mongoose.Schema.Types.ObjectId },
+    uuid: { type: String, default: uuidv4() },
     name: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
